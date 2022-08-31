2b6985c2cc0100ae18cd3d580f299ff4ae192871
bknower <knower.b@northeastern.edu>
2022-08-31 00:00:11 -0400

    Fixed messages

--- src/classes/NPC.ts
+++ src/classes/NPC.ts
@@ -1,17 +1,34 @@
+type Message = {
+  m: string;
+  cond?: () => boolean;
+  effect?: () => void;
+};
+
 export abstract class NPC {
   name: string;
   description: string;
-  msg: string = "";
+  messages: Message[] = [];
   constructor(name: string, description: string) {
     this.name = name;
     this.description = description;
   }
 
-  abstract hasMsg(): boolean;
+  getMsg() {
+    if (this.messages.length > 1) {
+      for (var i = 1; i < this.messages.length; i++) {
+        const msg = this.messages[i];
+        if (msg["cond"] && msg["cond"]()) {
+          if (msg["effect"]) {
+            msg["effect"]();
+          }
 
-  abstract getMsg(): string;
-
-  abstract getResponses(): string[] | undefined;
-
-  abstract answer(msg: string): void;
+          return msg["m"];
+        }
+      }
+    }
+    if (this.messages[0]["effect"]) {
+      this.messages[0]["effect"]();
+    }
+    return this.messages[0]["m"];
+  }
 }
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -20,31 +20,69 @@ import Stack from "@mui/material/Stack";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
 import { CardMedia } from "@mui/material";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 export const Person = ({ person }) => {
+  const [talkingTo, setTalkingTo] = useState(false);
   return (
-    <Grid item xs={12} sm={6} md={4} paddingRight="1rem">
-      <Box sx={{ minWidth: 250 }}>
-        <Card variant="outlined">
-          <>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 18, fontWeight: "bold", color: "textPrimary" }}
-                gutterBottom
-              >
-                {person.name}
-              </Typography>
-              <Typography variant="body2">{person.description}</Typography>
-            </CardContent>
-            <CardMedia
-              component="img"
-              image={require("../assets/people/Joseph_Aoun.jpg")}
-            />
-            <CardActions>
-              <Button size="small">Talk To</Button>
-            </CardActions>
-          </>
-        </Card>
-      </Box>
-    </Grid>
+    <>
+      <Grid item xs={12} sm={6} md={4} paddingRight="1rem">
+        <Box sx={{ minWidth: 250 }}>
+          <Card variant="outlined">
+            <>
+              <CardContent>
+                <Typography
+                  sx={{
+                    fontSize: 18,
+                    fontWeight: "bold",
+                    color: "textPrimary",
+                  }}
+                  gutterBottom
+                >
+                  {person.name}
+                </Typography>
+                <Typography variant="body2">{person.description}</Typography>
+              </CardContent>
+              <CardMedia
+                component="img"
+                image={require("../assets/people/Joseph_Aoun.jpg")}
+              />
+              <CardActions>
+                <Button size="small" onClick={() => setTalkingTo(true)}>
+                  Talk To
+                </Button>
+              </CardActions>
+            </>
+          </Card>
+        </Box>
+      </Grid>
+      <Modal
+        open={talkingTo}
+        onClose={() => setTalkingTo(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {person.name}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {person.getMsg()}
+          </Typography>
+        </Box>
+      </Modal>
+    </>
   );
 };
